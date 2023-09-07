4a07f90f689d1288a1b46cf62112e215a70f95f6
Talha Siddique <Talhasiddique375@gmail.com>
2023-09-07 16:39:12 +0200

    USeREDUCER hook is implemented to run the application and changing the UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import InitialDescription from "./InitialDescription";
+import Questions from "./Questions";
 
+const initialState = {
+  questions: [],
+  status: "Loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataArrived":
+      return { ...state, questions: action.payload, status: "dataArrived" };
+    case "dataFailed":
+      return { ...state, status: "dataFailed" };
+    case "active":
+      return { ...state, status: "active" };
+    default:
+      throw new Error("Unknown Error");
+  }
+}
 function App() {
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+  useEffect(function () {
+    fetch("http://localhost:9000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataArrived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "Loading" && <Loader />}
+        {status === "dataArrived" && (
+          <InitialDescription numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "dataFailed" && <Error />}
+        {status === "active" && <Questions />}
+      </Main>
     </div>
   );
 }
